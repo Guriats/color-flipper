70655f1998f237f27e936ba0718002322b5be60a
UsuarioM <espacioguriats@gmail.com>
2023-07-14 13:21:25 +0200

    color flipper

--- main.js
+++ main.js
@@ -8,50 +8,33 @@ const COLOR_PALETTE = {
   "#14248A": "Resolution Blue",
 };
 
-/*Object.keys(COLOR_PALETTE).forEach((color) => {
-  const option = document.createElement("option");
-  option.value = color;
-  option.innerText = COLOR_PALETTE[color];
-
-  colorPickerSelect.append(option);
-});*/
-
-
 const addOptionsToColorPicker = () => {
   const colorPickerSelect = document.querySelector("#color-picker");
+
   Object.keys(COLOR_PALETTE).forEach((color) => {
-  const option = document.createElement("option");
-  option.value = color;
-  option.innerText = COLOR_PALETTE[color];
-  colorPickerSelect.append(option);
-  })
-  }
-  addOptionsToColorPicker()
+    const option = document.createElement("option");
+    option.value = color;
 
- /* const addEventListenerToColorPicker = () => {
-    const colorPickerSelect = document.querySelector("#color-picker");
-    colorPickerSelect.addEventListener("change", (event) => {
-    const newColor = event.target.value;
-    //Almacenamos el código de color
-    document.body.style.backgroundColor = newColor;
-    //Le aplicamos el background color con el código de color seleccionado
-    });
-    };
-    addOptionsToColorPicker();
-    addEventListenerToColorPicker();*/
+    option.innerText = COLOR_PALETTE[color];
 
-    const addEventListenerToColorPicker = () => {
-      const colorPickerSelect = document.querySelector("#color-picker");
-      const colorName = document.querySelector("#color-name");
-      colorPickerSelect.addEventListener("change", (event) => {
-      const newColor = event.target.value;
-      document.body.style.backgroundColor = newColor;
-      //Le asignamos a nuestro h2 el valor de la clave que hay almacenada en newColor al igual que hemos hecho en la primera función
-      colorName.innerText = COLOR_PALETTE[newColor];
-      });
-      };
-      addOptionsToColorPicker();
-      addEventListenerToColorPicker();
+    colorPickerSelect.append(option);
+  });
+};
 
-      const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`
+
+
+const addEventListenerToColorPicker = () => {
+  const colorPickerSelect = document.querySelector("#color-picker");
+  const colorName = document.querySelector("#color-name");
+  colorPickerSelect.addEventListener("change", (event) => {
+  const newColor = event.target.value;
+  document.body.style.backgroundColor = newColor;
+  //Le asignamos a nuestro h2 el valor de la clave que hay almacenada en newColor al igual que hemos hecho en la primera función
+  colorName.innerText = COLOR_PALETTE[newColor];
+  });
+  };
+  addOptionsToColorPicker();
+  addEventListenerToColorPicker();
+
+const colorNameText = `${COLOR_PALETTE[newColor]} | ${newColor}`;
 colorName.innerText = COLOR_PALETTE[newColor] ? colorNameText : "-";
